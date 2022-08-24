84f3eba63bbfff49d979ddbd81698e8053599a39
Your Name <naimur202163@gmail.com>
2022-08-24 10:13:51 +0600

    project done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello From Console.log");
+const button = document
+  .querySelector(".toogle")
+  .addEventListener("click", () => {
+    console.log("Naimur Rahman");
+
+    document.querySelector(".mobile-nav img ").style.display = "none";
+    document.querySelector(".toogle img").style.display = "none";
+    document.querySelector(".nav-bar").style.display="block"
+  });
